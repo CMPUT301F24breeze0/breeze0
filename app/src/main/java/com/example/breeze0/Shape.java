c8f22d15e9487d690e5066e40ac8c2aa6015a7bf
yuhui15 <155590543+yuhui15@users.noreply.github.com>
2024-09-25 18:37:47 -0600

    Update Shape.java

--- app/src/main/java/com/example/breeze0/Shape.java
+++ app/src/main/java/com/example/breeze0/Shape.java
@@ -1,34 +1,31 @@
+
 package com.example.breeze0;
 
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "blue"; // Replace with your chosen color when resolving conflicts.
-
-    // Constructor
+    String color = "red"; 
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
 
-    // Getters
+
     public Integer getX() {
         return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
     }
 
     public Integer getY() {
         return y;
     }
 
-    // Setters
-    public void setX(Integer x) {
-        this.x = x;
-    }
-
     public void setY(Integer y) {
         this.y = y;
     }
-
-    // Abstract method to be implemented by subclasses
-    public abstract void draw(); 
+    
+    public abstract void draw();
 }
