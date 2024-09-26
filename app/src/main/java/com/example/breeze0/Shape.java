685117eba506906d36173970959dc4c092577375
Zilin Qiu <113585506+zq6UA@users.noreply.github.com>
2024-09-25 18:38:14 -0600

    Update Shape.java

--- app/src/main/java/com/example/breeze0/Shape.java
+++ app/src/main/java/com/example/breeze0/Shape.java
@@ -7,5 +7,5 @@ import android.os.Bundle;
 public abstract class Shape{
     private Integer x;
     private Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "blue";
 }
