cbdaa085c649025124915321b32503b5ab119624
WilliamGuo2002 <yg17@ualberta.ca>
2024-09-25 18:21:02 -0600

    Created project

---
+++ app/src/main/java/com/example/breeze0/Shape.java
@@ -0,0 +1,11 @@
+package com.example.breeze0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public abstract class Shape{
+    private Integer x;
+    private Integer y;
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+}
