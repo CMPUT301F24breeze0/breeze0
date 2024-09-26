98e9d79c83afbdde13440707fb11f48cd7e0fe6b
anyname-ui <ericluosk@gmail.com>
2024-09-25 21:07:34 -0400

    i submit

---
+++ app/src/main/java/com/example/breeze0/Rectangle.java
@@ -0,0 +1,11 @@
+package com.example.breeze0;
+
+public class Rectangle extends Shape{
+    private Integer length;
+    private Integer width;
+    public Rectangle(Integer length,Integer width){
+        this.length=length;
+        this.width=width;
+    }
+
+}
